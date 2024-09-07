382727e9834053c9b31a834cc1c0700c57c6d18c
mak2220 <bertamarcosesteban@gmail.com>
2024-09-07 16:52:25 -0300

    Cambios en index.tsx fecha 07/09/24

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,19 @@
 import Head from "next/head";
 import clientPromise from "../lib/mongodb";
 import { useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import styles from "../styles/index.module.css";
-import Navbar from "../component/Navbar";
 import Link from "next/link";
 import Footer from "../component/Footer";
-require('../eventsConfig');
+import Navbar from "../component/Navbar";
 
-type ConnectionStatus = {
-  isConnected: boolean;
-};
-
-export const getServerSideProps: GetServerSideProps<
-  ConnectionStatus
-> = async () => {
+export const getStaticProps: GetServerSideProps<ConnectionStatus> = async () => {
   try {
     await clientPromise;
     
     return {
       props: { isConnected: true },
+      revalidate: 60,  // ISR: Revalida cada 60 segundos
     };
   } catch (e) {
     console.error(e);
@@ -33,8 +26,14 @@ export const getServerSideProps: GetServerSideProps<
 export default function Home({
   isConnected,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+  // Mueve el hook dentro del componente funcional
   useEffect(() => {
-    alert(' Bienvenido a Chajari Wine. Esta aplicación es solo con fines de propaganda y divulgación, no expresa ningún tipo de compromiso comercial o promesa de prestación de servicos de ningún tipo');
+    // Cargar Bootstrap en el cliente
+    if (typeof window !== "undefined") {
+      require('bootstrap/dist/css/bootstrap.min.css');
+      alert('Bienvenido a Chajari Wine. Esta aplicación es solo con fines de propaganda y divulgación, no expresa ningún tipo de compromiso comercial o promesa de servicios de ningún tipo');
+    }
   }, []);
     
   return (
@@ -42,24 +41,11 @@ export default function Home({
       <Head>
         <title>ChajaríWine</title>
         <link rel="icon" href="favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <meta name="msapplication-TileColor" content="#ffffff"/>
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
         <meta name="theme-color" content="#ffffff"></meta>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </Head>
+      
       <main className={styles.main}>
         <Navbar />
 
@@ -74,16 +60,24 @@ export default function Home({
             </h2>
           </section>
         )}
+        
         <section className={styles.containerImg}>
-          <img src="/wineLogo.png" alt="ChajaríWine" className={styles.img}></img>
-      </section>
-      <section className={styles.subtitle}>
-        <Link href={'/vinos'} className={styles.link}>
-          <h3>Conoce nuestros vinos</h3>
-        </Link>
-      </section>
-    <Footer />
-    </main>
+          <img
+            src="/wineLogo.png"
+            alt="ChajaríWine"
+            className={styles.img}
+            loading="lazy"  // Carga diferida de la imagen
+          />
+        </section>
+        
+        <section className={styles.subtitle}>
+          <Link href="/vinos" className={styles.link}>
+            <h3>Conoce nuestros vinos</h3>
+          </Link>
+        </section>
+        
+        <Footer />
+      </main>
     </div>
   );
 }
